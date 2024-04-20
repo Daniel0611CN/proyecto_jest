413a4201b5d497ac2f57dd02c1d8fccd18984dca
Daniel Clavijo Núñez <148864537+Daniel0611CN@users.noreply.github.com>
2024-04-20 12:42:43 +0200

    Jest Test

--- jest.test.js
+++ jest.test.js
@@ -1,23 +1,20 @@
 //https://jestjs.io/docs/using-matchers
 
-const sum = require('./sum');
+const sum = require("./sum");
 
 //Common Matchers
-test('adds 1 + 2 to equal 3', () => {
-
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
-
 });
 
-it('object assignment', () => {
-  const data = {one: 1};
-  data['two'] = 2;
-  expect(data).toEqual({one: 1, two: 2});
-  expect(data).not.toBe({one: 1, two: 2});
- 
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+  expect(data).not.toBe({ one: 1, two: 2 });
 });
 
-it('adding positive numbers is not zero', () => {
+test("adding positive numbers is not zero", () => {
   for (let a = 1; a < 10; a++) {
     for (let b = 1; b < 10; b++) {
       expect(a + b).not.toBe(0);
@@ -26,7 +23,7 @@ it('adding positive numbers is not zero', () => {
 });
 
 //Truthiness
-it('null', () => {
+test("null", () => {
   const n = null;
   expect(n).toBeNull();
   expect(n).toBeDefined();
@@ -35,7 +32,7 @@ it('null', () => {
   expect(n).toBeFalsy();
 });
 
-test('zero', () => {
+test("zero", () => {
   const z = 0;
   expect(z).not.toBeNull();
   expect(z).toBeDefined();
@@ -45,7 +42,7 @@ test('zero', () => {
 });
 
 //Numbers
-test('two plus two', () => {
+test("two plus two", () => {
   const value = 2 + 2;
   expect(value).toBeGreaterThan(3);
   expect(value).toBeGreaterThanOrEqual(3.5);
@@ -57,46 +54,46 @@ test('two plus two', () => {
   expect(value).toEqual(4);
 });
 
-test('adding floating point numbers', () => {
+test("adding floating point numbers", () => {
   const value = 0.1 + 0.2;
   //expect(value).toBe(0.3);           This won't work because of rounding error
   expect(value).toBeCloseTo(0.3); // This works.
 });
 
 //String
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/);
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
 });
 
 test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
+  expect("Christoph").toMatch(/stop/);
 });
 
 //Arrays and Iterables
 const shoppingList = [
-  'diapers',
-  'kleenex',
-  'trash bags',
-  'paper towels',
-  'milk',
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
 ];
 
-test('the shopping list has milk on it', () => {
-  expect(shoppingList).toContain('milk');
-  expect(new Set(shoppingList)).toContain('milk');
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
 });
 
 //Exceptions
 function compileAndroidCode() {
-  throw new Error('you are using the wrong JDK!');
+  throw new Error("you are using the wrong JDK!");
 }
 
-test('compiling android goes as expected', () => {
+test("compiling android goes as expected", () => {
   expect(() => compileAndroidCode()).toThrow();
   expect(() => compileAndroidCode()).toThrow(Error);
 
   // You can also use a string that must be contained in the error message or a regexp
-  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
   expect(() => compileAndroidCode()).toThrow(/JDK/);
 
   // Or you can match an exact error message using a regexp like below
@@ -107,65 +104,65 @@ test('compiling android goes as expected', () => {
 //More...
 //https://jestjs.io/docs/expect
 //Promises
-test('resolves to lemon', () => {
+test("resolves to lemon", () => {
   // make sure to add a return statement
-  return expect(Promise.resolve('lemon')).resolves.toBe('lemon');
+  return expect(Promise.resolve("lemon")).resolves.toBe("lemon");
 });
 
-test('rejects to octopus', () => {
+test("rejects to octopus", () => {
   // make sure to add a return statement
-  return expect(Promise.reject(new Error('octopus'))).rejects.toThrow(
-    'octopus',
+  return expect(Promise.reject(new Error("octopus"))).rejects.toThrow(
+    "octopus"
   );
 });
 
 //Object
 const can = {
-  name: 'pamplemousse',
+  name: "pamplemousse",
   ounces: 12,
 };
 
-describe('the can', () => {
-  test('has 12 ounces', () => {
+describe("the can", () => {
+  test("has 12 ounces", () => {
     expect(can.ounces).toBe(12);
   });
 
-  test('has a sophisticated name', () => {
-    expect(can.name).toBe('pamplemousse');
+  test("has a sophisticated name", () => {
+    expect(can.name).toBe("pamplemousse");
   });
 });
 
 function drinkAll(callback, flavour) {
-  if (flavour !== 'octopus') {
+  if (flavour !== "octopus") {
     callback(flavour);
   }
 }
 
 //Callbacks
-describe('drinkAll', () => {
-  test('drinks something lemon-flavoured', () => {
+describe("drinkAll", () => {
+  test("drinks something lemon-flavoured", () => {
     const drink = jest.fn();
-    drinkAll(drink, 'lemon');
+    drinkAll(drink, "lemon");
     expect(drink).toHaveBeenCalled();
   });
 
-  test('does not drink something octopus-flavoured', () => {
+  test("does not drink something octopus-flavoured", () => {
     const drink = jest.fn();
-    drinkAll(drink, 'octopus');
+    drinkAll(drink, "octopus");
     expect(drink).not.toHaveBeenCalled();
   });
 });
 
 function drinkEach(callback, flavours) {
-  for( let i = 0; i < flavours.length; i++) {
-    if (flavours[i] !== 'octopus') {
-    callback(flavours[i]);
+  for (let i = 0; i < flavours.length; i++) {
+    if (flavours[i] !== "octopus") {
+      callback(flavours[i]);
     }
   }
 }
 
-test('drinkEach drinks each drink', () => {
+test("drinkEach drinks each drink", () => {
   const drink = jest.fn();
-  drinkEach(drink, ['lemon', 'octops']);
+  drinkEach(drink, ["lemon", "octops"]);
   expect(drink).toHaveBeenCalledTimes(2);
 });
